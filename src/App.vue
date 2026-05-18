ee89d729dfae8b47b238281c5305bc97bd9716aa
beanbox08 <beanbox08@gmail.com>
2026-05-18 15:56:49 +0900

    기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted, ref, type Ref } from 'vue'
-
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
+import { computed, onMounted, ref, useTemplateRef, type Ref } from 'vue'
 
 type TDJson = {
   timestamp: number,
@@ -30,6 +27,11 @@ const targetDatetimeString = computed(() => {
   const weekday = (new Date(`${year}-${month}-${day}`)).toLocaleDateString(locale.value, { weekday: "short" })
   return `${year}-${month}-${day} (${weekday})`
 })
+const targetItemList = computed(() => {
+  const datetime = targetDatetime.value
+  return itemList.value.filter(({ datetime: d }) => d.year === datetime.year && d.month === datetime.month && d.day === datetime.day).sort((d1, d2) => d1.datetime.hour > d2.datetime.hour ? -1 : 1)
+})
+const inputTargetDate = useTemplateRef('input-target-date')
 
 onMounted(() => {
   try {
@@ -90,7 +92,29 @@ function calcDatetime(_d: number | Date) {
   return datetime
 }
 
-async function onAddItem() {
+function moveTargetDate(type: 'day', amount: number) {
+  if (type === 'day') {
+    clearModifyingList()
+    const { year, month, day } = targetDatetime.value
+    const newTargetDate = new Date(`${year}-${month}-${day}`)
+    newTargetDate.setDate(newTargetDate.getDate() + amount)
+    targetDatetime.value = calcDatetime(newTargetDate)
+  }
+}
+
+function onTargetDateInput() {
+  const inputDate = inputTargetDate.value?.valueAsDate
+  console.log('onTargetDateInput', inputDate)
+  if (inputDate && !isNaN(inputDate.getTime())) {
+    clearModifyingList()
+    const { year, month, day } = calcDatetime(inputDate)
+    targetDatetime.value = {
+      year, month, day
+    }
+  }
+}
+
+function onAddItem() {
   const { value } = newItemTitle
   if (!value.trim()) return
 
@@ -98,24 +122,88 @@ async function onAddItem() {
   const now = new Date()
   const datetime = calcDatetime(now)
 
-  let oldItemIndex = -1
-  if ((oldItemIndex = itemList.value.findIndex((({ datetime: d }) => d.year === datetime.year && d.month === datetime.month && d.day === datetime.day && d.hour === datetime.hour))) >= 0) {
-    itemList.value[oldItemIndex]!.title += ` ${value}`
-  } else {
-    newItem.datetime = datetime
-    newItem.title = value
-    itemList.value.push(newItem as TDItem)
+  newItem.datetime = datetime
+  newItem.title = value
+
+  insertItem(newItem as TDItem)
+
+  newItemTitle.value = ''
+}
+function onRemoveItem(datetime: TDDatetime) {
+  let idx = -1
+  if ((idx = getItemIndex(datetime)) >= 0) {
+    itemList.value.splice(idx, 1)
   }
 
   setItemFromStorage({ itemList: itemList.value })
 
   newItemTitle.value = ''
 }
+function getItemIndex(datetime: TDDatetime) {
+  return itemList.value.findIndex((({ datetime: d }) => d.year === datetime.year && d.month === datetime.month && d.day === datetime.day && d.hour === datetime.hour))
+}
+function insertItem(item: TDItem) {
+  let idx = -1
+  const { datetime: key, ...itemValue } = item
+  if ((idx = getItemIndex(key)) >= 0) {
+    const newTitle = `${itemList.value[idx]!.title} ${item.title}`
+    itemList.value[idx] = { ...itemList.value[idx]!, ...itemValue }
+    itemList.value[idx]!.title = newTitle
+  } else {
+    const newItem: TDItem = { ...item }
+    itemList.value.push(newItem)
+  }
+  setItemFromStorage({ itemList: itemList.value })
+}
+function updateItem(item: TDItem) {
+  let idx = -1
+  const { datetime: key, ...itemValue } = item
+  if ((idx = getItemIndex(key)) >= 0) {
+    itemList.value[idx] = { ...itemList.value[idx]!, ...itemValue }
+    setItemFromStorage({ itemList: itemList.value })
+  }
+}
+
+// ===
+// 수정 모드
+// ===
+type TDModifyingItem = Partial<TDItem> & Pick<TDItem, 'datetime'>
+const modifyingList = ref<Array<TDModifyingItem>>([])
+function clearModifyingList() { modifyingList.value = [] }
+function toggleModifyingList(datetimeData: TDModifyingItem) {
+  let idx = -1
+  const datetime = datetimeData.datetime
+  if ((idx = getModifyingIndex(datetime)) >= 0) {
+    updateItem({ datetime, title: datetimeData.title ?? '' })
+
+    modifyingList.value.splice(idx, 1)
+  } else {
+    modifyingList.value.push({ datetime, title: datetimeData.title })
+  }
+}
+function getModifyingIndex(datetime: TDDatetime) {
+  return modifyingList.value.findIndex((({ datetime: d }) => d.year === datetime.year && d.month === datetime.month && d.day === datetime.day && d.hour === datetime.hour))
+}
+function isModifying(datetime: TDDatetime) { return getModifyingIndex(datetime) >= 0 }
+function getModifyingItem(datetimeData: TDModifyingItem) {
+  let idx = -1
+  const datetime = datetimeData.datetime
+  if ((idx = getModifyingIndex(datetime)) >= 0) {
+    return modifyingList.value[idx]!
+  }
+}
+
+
 </script>
 
 <template>
-  <header>
-    {{ targetDatetimeString }}
+  <header :style="{ display: 'inline-flex', gap: '4px' }">
+    <button type="button" @click="moveTargetDate('day', -1)"> &lt; </button>
+    <span>
+      <input ref="input-target-date" type="date" style="width: 0; opacity: 0;" @input="onTargetDateInput" />
+      <span @click="inputTargetDate?.showPicker?.()" :style="{ cursor: 'pointer' }">{{ targetDatetimeString }}</span>
+    </span>
+    <button type="button" @click="moveTargetDate('day', +1)"> &gt; </button>
   </header>
 
   <main>
@@ -124,39 +212,20 @@ async function onAddItem() {
       <button type="button" @click="onAddItem">+</button>
     </div>
     <div>
-      <div v-for="item in itemList" :style="{ display: 'inline-flex', gap: '4px' }">
+      <div v-for="item in targetItemList" :style="{ display: 'flex', gap: '4px' }">
         <span>{{ item.datetime.hour }}:{{ '00' }}</span>
-        <span>{{ item.title }}</span>
+        <template v-if="isModifying(item.datetime)">
+          <input type="text" v-model="getModifyingItem(item)!.title!" />
+          <button type="button" @click="toggleModifyingList(getModifyingItem(item)!)">✅</button>
+        </template>
+        <template v-else>
+          <span>{{ item.title }}</span>
+          <button type="button" @click="toggleModifyingList(item)">📝</button>
+        </template>
+        <button type="button" @click="onRemoveItem(item.datetime)">❌</button>
       </div>
     </div>
   </main>
 </template>
 
-<style scoped>
-/* header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-} */
-</style>
+<style scoped></style>
